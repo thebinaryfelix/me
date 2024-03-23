567c66037de81f584c5e64d6b07c2a3ae5df7c39
thebinaryfelix <thebinaryfelix@gmail.com>
2024-03-23 12:52:02 -0300

    refactor(SkillCard): improve browser detection

--- src/client/components/atoms/SkillCard/SkillCard.tsx
+++ src/client/components/atoms/SkillCard/SkillCard.tsx
@@ -1,13 +1,8 @@
 "use client";
 
-import { useIsClient } from "@/client/hooks";
+import { useBrowser, useIsClient } from "@/client/hooks";
 import { sendGAEvent } from "@next/third-parties/google";
 import React, { useEffect, useRef, useState } from "react";
-
-// To avoid a bug related with backface-visibility on Firefox we have to bypass this CSS property
-// Reference: https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browsers
-// @ts-ignore
-const isFirefox = typeof InstallTrigger !== 'undefined';
 
 interface SkillCardProps {
   Icon: React.JSX.Element;
@@ -20,7 +15,9 @@ export const SkillCard = ({ Icon, tags, title }: SkillCardProps) => {
 
   const [rotateCard, setRotateCard] = useState(false);
 
-  const { isClient } = useIsClient()
+  const { isClient } = useIsClient();
+
+  const { isFirefox } = useBrowser();
 
   const ref = useRef(null);
 
@@ -41,9 +38,13 @@ export const SkillCard = ({ Icon, tags, title }: SkillCardProps) => {
 
   const rotateCardClass = rotateCard ? "my-rotate-y-180" : "";
 
-  const browserDependantClasses = isFirefox ?
-    ["transition-[opacity]", "delay-[250ms]", `${rotateCard ? "opacity-0" : "opacity-100"}`].join(" ")
-    : "backface-hidden"
+  const browserDependantClasses = isFirefox
+    ? [
+        "transition-[opacity]",
+        "delay-[250ms]",
+        `${rotateCard ? "opacity-0" : "opacity-100"}`,
+      ].join(" ")
+    : "backface-hidden";
 
   const handleToggleRotate = () => {
     setRotateCard((state) => !state);
@@ -71,7 +72,9 @@ export const SkillCard = ({ Icon, tags, title }: SkillCardProps) => {
       <div
         className={`relative preserve-3d ${rotateCardClass} w-full h-[115px] duration-1000 p-4 rounded-lg border-solid border-[1px] border-[#C9C9C9] dark:border-[#334D66] bg-[#E8EDF2] dark:bg-[#1A2633]`}
       >
-        <div className={`${browserDependantClasses} w-full h-full flex flex-col justify-center align-middle items-center`}>
+        <div
+          className={`${browserDependantClasses} w-full h-full flex flex-col justify-center align-middle items-center`}
+        >
           {Icon}
           <p className="font-medium mt-3 text-primary-dark dark:text-primary-light md:text-lg">
             {title}
@@ -79,7 +82,9 @@ export const SkillCard = ({ Icon, tags, title }: SkillCardProps) => {
         </div>
 
         <div className="backface-hidden my-rotate-y-180 absolute top-0 left-0 p-4 w-full h-full overflow-hidden flex justify-center items-center text-primary-dark dark:text-primary-light">
-          <p className="text-center text-xs md:text-base lg:text-lg">{tags.join(", ")}</p>
+          <p className="text-center text-xs md:text-base lg:text-lg">
+            {tags.join(", ")}
+          </p>
         </div>
       </div>
     </div>

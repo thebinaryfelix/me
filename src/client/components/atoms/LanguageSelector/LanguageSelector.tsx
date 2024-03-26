e0746a08f3fa2dc7929c3891a6cd0eeec3f6b924
Mateus Felix <mateus.cfoliveira@gmail.com>
2024-03-26 20:10:05 -0300

    refactor(LanguageSelector): update language labels (#15)
    and menu alignment

--- src/client/components/atoms/LanguageSelector/LanguageSelector.tsx
+++ src/client/components/atoms/LanguageSelector/LanguageSelector.tsx
@@ -4,9 +4,9 @@ import { usePathname, useRouter } from 'next/navigation'
 import { useEffect, useRef, useState } from 'react'
 
 const languages = [
-  { code: 'en', name: 'en', flagCode: 'us' },
-  { code: 'pt', name: 'pt-BR', flagCode: 'br' },
-  { code: 'es', name: 'es', flagCode: 'es' },
+  { code: 'en', label: 'English', flagCode: 'us' },
+  { code: 'pt', label: 'Português', flagCode: 'br' },
+  { code: 'es', label: 'Español', flagCode: 'es' },
 ]
 
 export const LanguageSelector = () => {
@@ -58,7 +58,7 @@ export const LanguageSelector = () => {
           className='flex justify-center items-center w-full rounded-md border border-gray-300 dark:border-slate-500 shadow-sm px-4 py-2 text-sm font-medium text-slate-800 dark:text-gray-400 hover:bg-slate-100 hover:dark:bg-slate-600 focus:outline-none'
         >
           <span className={`fi fi-${currentLanguage.flagCode} mr-1`} />
-          {currentLanguage?.name}{' '}
+          {currentLanguage?.label}{' '}
         </button>
       </div>
 
@@ -66,19 +66,19 @@ export const LanguageSelector = () => {
         <div
           role='menu'
           aria-orientation='vertical'
-          aria-labelledby='menu-button'
-          className='absolute right-0 z-10 w-28 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'
+          className='absolute left-0 z-10 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'
         >
           <div className='py-1' role='none'>
-            {languages.map(({ code, name, flagCode }) => (
+            {languages.map(({ code, label, flagCode }) => (
               <button
                 key={code}
-                className={`block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left ${code === currentLanguage.code ? 'font-bold' : ''}`}
+                aria-label={label}
+                className={`flex px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full ${code === currentLanguage.code ? 'font-bold' : ''}`}
                 role='menuitem'
                 onClick={() => handleChangeLanguage(code)}
               >
                 <span className={`fi fi-${flagCode} mr-1`} />
-                {name}
+                {label}
               </button>
             ))}
           </div>

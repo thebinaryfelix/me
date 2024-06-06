ce1060ccf25ae0dd8c0223b285a886661c452e2d
Mateus Félix <mateus.cfoliveira@gmail.com>
2024-06-06 17:51:50 -0300

    refactor(Footer): add github link to name on footer

--- src/server/components/molecules/Footer/Footer.tsx
+++ src/server/components/molecules/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import { TranslationProps } from '@/types/translation'
+import Link from 'next/link'
 
 export const Footer = ({ dictionary }: TranslationProps) => {
   const year = new Date().getFullYear()
@@ -7,6 +8,13 @@ export const Footer = ({ dictionary }: TranslationProps) => {
     <footer id='footer' className='mt-10'>
       <p className='w-full flex justify-center text-primary-dark dark:text-primary-light'>
         © {year}. {dictionary.home.footer.copyright.text}
+        <Link
+          href='https://github.com/thebinaryfelix'
+          target='_blank'
+          className='ml-1 underline'
+        >
+          Mateus Felix
+        </Link>
       </p>
     </footer>
   )
